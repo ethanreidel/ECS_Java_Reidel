11041594950cf638a0a0eedb753fc805186916a2
EthanReidel <ReidelE8542@sduhsd.lan>
2017-12-01 10:46:11 -0800

    Sync

---
+++ YourInitials/src/Initials.java
@@ -0,0 +1,21 @@
+
+public class Initials 
+{
+	
+
+	public static void main (String[] args)
+	{
+	System.out.println("EEEEE        JJJJJ       RRRR      ");	
+	System.out.println("E              J         R   R    ");
+	System.out.println("EEEEE          J         R   R    ");
+	System.out.println("E              J         RRRR       ");
+	System.out.println("E          J   J         R R      ");
+	System.out.println("EEEEE        J           R   R    ");
+	
+	
+	
+	
+	
+	
+	}
+}
