ebe160ac65611ef839b3e06b513cb3647d0701a1
EthanReidel <ReidelE8542@sduhsd.lan>
2017-12-04 10:52:50 -0800

    jsrkljklrghjkla

--- MoreVaariablesAndPrinting.java/src/MoreVariablesAndPrinting.java
+++ MoreVaariablesAndPrinting.java/src/MoreVariablesAndPrinting.java
@@ -18,7 +18,7 @@ public class MoreVariablesAndPrinting
 		myHair = "Brown";
 		
 		System.out.println( "Let's talk about " + myName + ".");
-		System.out.println("He's " + myHeight + "or " + myHeightCm  + "cm"  "inches tall." );
+		System.out.println("He's " + myHeight + "or " + myHeightCm  + "cm"  );
 		System.out.println( "He's " + myWeight + " pounds heavy." );
 		System.out.println( "Actually, that's not too heavy.");
 		System.out.println( "He's got " + myEyes + " eyes and " + myHair + "hair." );
